8ce2308af03f29a3eded72d96a091fe1a5968424
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-11-25 22:13:26 +0100

    hide using gpio

--- gpio/power_switch.go
+++ gpio/power_switch.go
@@ -2,15 +2,49 @@ package gpio
 
 import (
 	"github.com/stianeikeland/go-rpio"
+	"log"
 	"time"
 )
 
-const (
-	WaitTime time.Duration = time.Second / 4
-)
+type PowerSwitch interface {
+	TurnOnPc()
+	Close() error
+}
 
-func TurnOnPc(pin rpio.Pin) {
-	pin.High()
-	time.Sleep(WaitTime)
-	pin.Low()
+type gpioPowerSwitch struct {
+	logger   *log.Logger
+	pin      rpio.Pin
+	waitTime time.Duration
+}
+
+func NewGpioPowerSwitch(logger *log.Logger, pinNumber int, waitTime time.Duration) (PowerSwitch, error) {
+	err := rpio.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	pin := rpio.Pin(pinNumber)
+	pin.Output()
+
+	return gpioPowerSwitch{
+		logger:   logger,
+		pin:      pin,
+		waitTime: waitTime,
+	}, nil
+}
+
+func (powerSwitch gpioPowerSwitch) TurnOnPc() {
+	powerSwitch.pin.High()
+	time.Sleep(powerSwitch.waitTime)
+	powerSwitch.pin.Low()
+}
+
+func (powerSwitch gpioPowerSwitch) Close() error {
+	err := rpio.Close()
+
+	if err != nil {
+		powerSwitch.logger.Println("error: ", err.Error())
+	}
+
+	return err
 }
